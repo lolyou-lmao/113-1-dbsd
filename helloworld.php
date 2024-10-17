b6019bc23a432e29cfbdc728d108986a547fc6fb
lolyou-lmao <denil0126@gmail.com>
2024-10-17 11:09:45 +0800

    codes

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<?php
+//start value
+$begin=12;
+//finish value
+$end=18;
+//for loop to run $b to $end
+for($i=$begin; $i<=$end; $i+=3){
+?>
+<!-- p these evertime loop runs -->
+my first php page c111181116 <BR>
+
+<?php
+}
+?>
